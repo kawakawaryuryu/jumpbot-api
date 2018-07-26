12a9005aa6d13ed827d481419240e721a2e7f259
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2018-07-26 22:05:54 +0900

    fix jump controller

--- src/main/kotlin/jp/co/kawakawaryuryu/jumpbot/jumpbotweb/jump/JumpController.kt
+++ src/main/kotlin/jp/co/kawakawaryuryu/jumpbot/jumpbotweb/jump/JumpController.kt
@@ -4,14 +4,18 @@ import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @RestController
-class JumpController {
+class JumpController(
+        val jumpService: JumpService
+) {
 
     @GetMapping(value = "/jump", consumes = [MediaType.APPLICATION_JSON_UTF8_VALUE],
             produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
     fun getJump(@RequestParam date: String): JumpResource {
-        // TODO
+        jumpService.getJump(LocalDate.parse(date, DateTimeFormatter.BASIC_ISO_DATE))
         return JumpResource("20180724", 100, false)
     }
 }
